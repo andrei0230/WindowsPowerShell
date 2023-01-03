4205dafeac7945d56683cce209ef3e8b4f94c9e3
Andrei Ciucle <andrei.ciucle02@gmail.com>
2023-01-03 12:50:59 +0200

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+# Custom powershell prompt
+# By Andrei C.
+
+# Custom variables
+$user = $env:USERNAME
+$computer = $env:COMPUTERNAME
+$chevron = [char]0xf054
+
+# Prompt function
+function prompt 
+{
+    $path = (''+$PWD).Replace($HOME,"~")
+
+    Write-Host "$user@$computer" -ForegroundColor Green -NoNewline
+    Write-Host ":" -NoNewline
+    Write-Host "[$path] " -ForegroundColor Blue -NoNewline
+    Write-Host "$chevron" -ForegroundColor Yellow -NoNewline
+    return " "    
+}
